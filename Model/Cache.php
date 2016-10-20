b85392ec88edefdd68a78b682dd0f185bdcb31e5
Alex Wang <selltrends@gmail.com>
2016-10-20 14:56:48 +0800

    add cache function

---
+++ Model/Cache.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Atopt\Office\Model;
+
+class Cache extends \Magento\Framework\Cache\Frontend\Decorator\TagScope
+{
+    const TYPE_IDENTIFIER = 'atopt_office';
+    const CACHE_TAG = 'OFFICE';
+    public function __construct(
+        \Magento\Framework\App\Cache\Type\FrontendPool $cacheFrontendPool
+    )
+    {
+        parent::__construct(
+            $cacheFrontendPool->get(self::TYPE_IDENTIFIER), self::CACHE_TAG
+        );
+    }
+}
