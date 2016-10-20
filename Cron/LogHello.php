82e4ca2f70f7ab9701100ac4415668d02e7f9640
Alex Wang <selltrends@gmail.com>
2016-10-20 14:47:40 +0800

    add crontab function

---
+++ Cron/LogHello.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Atopt\Office\Cron;
+
+class LogHello
+{
+    protected $logger;
+    public function __construct(
+        \Psr\Log\LoggerInterface $logger
+    )
+    {
+        $this->logger = $logger;
+    }
+    public function execute()
+    {
+        $this->logger->info('Hello from Cron job!');
+        return $this;
+    }
+}
