97648279a91e27c0c7277baa3d2bf9a8d80385ca
Alex Wang <selltrends@gmail.com>
2016-10-20 14:53:01 +0800

    add event & oberver function

---
+++ Observer/LogCustomerEmail.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Atopt\Office\Observer;
+
+use Magento\Framework\Event\ObserverInterface;
+
+class LogCustomerEmail implements ObserverInterface
+{
+    protected $logger;
+    public function __construct(
+        \Psr\Log\LoggerInterface $logger
+    )
+    {
+        $this->logger = $logger;
+    }
+    /**
+     * Just some test code to log customer email on login
+     *
+     * @param \Magento\Framework\Event\Observer $observer
+     * @return self
+     */
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+        //$password = $observer->getEvent()->getPassword();
+        //$customer = $observer->getEvent()->getModel();
+        //$this->logger->info('Atopt\Office: ' . $customer->getEmail());
+        return $this;
+    }
+}
